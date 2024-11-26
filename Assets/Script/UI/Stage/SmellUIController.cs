7c42db502f6dc36a5d7e8178c212cce64c923d37
puyoco <93511276+puyoco@users.noreply.github.com>
2024-11-26 14:08:25 +0900

    ちょい調整
    センターUiのプレファブ化、
    SmellUIController.csアップデート

--- Assets/Script/UI/Stage/SmellUIController.cs
+++ Assets/Script/UI/Stage/SmellUIController.cs
@@ -24,11 +24,11 @@ public class SmellUIController : MonoBehaviour
         if (IsSmelled == false)
         {
             //“õ‚¢‚ðšk‚®ƒ{ƒ^ƒ“
-            if (Input.GetButtonDown("SmellKey"))
+            if (Input.GetButtonDown("SmellKey")||Input.GetKeyDown(KeyCode.F))
             {
                 Debug.Log("StartSmell");
             }
-            if (Input.GetButton("SmellKey"))
+            if (Input.GetButton("SmellKey") || Input.GetKey(KeyCode.F))
             {
                 Smell_UI.DOFillAmount(1.2f, FillDuration);
                 if (Smell_UI.fillAmount >= 0.99f)
@@ -43,7 +43,7 @@ public class SmellUIController : MonoBehaviour
             }
 
         }
-        if (Input.GetButtonUp("SmellKey"))
+        if (Input.GetButtonUp("SmellKey") || Input.GetKeyUp(KeyCode.F))
         {
             Debug.Log("SmellOver");
             Smell_UI.DOFillAmount(0f, FillDuration);
