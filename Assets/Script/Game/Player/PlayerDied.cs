21253a63c6490f4a5fec6008dfd1c136b72c6e01
puyoco <93511276+puyoco@users.noreply.github.com>
2025-01-28 16:37:49 +0900

    Update PlayerDied.cs

--- Assets/Script/Game/Player/PlayerDied.cs
+++ Assets/Script/Game/Player/PlayerDied.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerDied : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy")
         {
