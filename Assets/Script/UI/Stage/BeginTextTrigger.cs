09b981229fc09691b5a6bbbb73d3dfe71ab176e9
puyoco <93511276+puyoco@users.noreply.github.com>
2025-01-28 11:56:27 +0900

    BeginTextの様式変更

--- Assets/Script/UI/Stage/BeginTextTrigger.cs
+++ Assets/Script/UI/Stage/BeginTextTrigger.cs
@@ -1,22 +1,30 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BeginTextCollider : MonoBehaviour
+public class BeginTextTrigger : MonoBehaviour
 {
     public Image BeginningText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            BeginningText.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
